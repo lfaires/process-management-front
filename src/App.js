0b4973f42c2d3aac28d9e41d710e1c0cb795ff85
Luiz Fernando <lfaires@gmail.com>
2021-09-03 01:41:14 -0300

    chore: client and process form routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
+import ProcessForm from './pages/ProcessForm';
+import ClientForm from './pages/ClientForm'
 
 export default function App() {
   return (
     <BrowserRouter>
         <Switch>
           <Route component={Home} path="/" exact />
+          <Route component={ProcessForm} path="/register-process" exact />
+          <Route component={ClientForm} path="/register-client" exact />
         </Switch>
     </BrowserRouter>
   );
